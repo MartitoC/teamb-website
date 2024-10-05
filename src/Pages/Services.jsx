c0f874cf9c89f47f0baffc9a0bc30f4b6cea60b2
Martin <martin.cardozo500@gmail.com>
2024-10-05 03:35:25 -0400

    modificacion card y services

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -9,18 +9,15 @@ import supportImg from '../assets/support.png';
 
 const Services = () => {
   return (
-    <div className="w-3/4 mx-auto">
-        <div id="services" className="flex justify-center items-center mb-24 text-4xl text-gray-900 font-semibold">
-            Servicios
-        </div>
-        <div className="grid grid-cols-3 gap-4 justify-center items-center"> 
-            <Card title="Software" image={softwareImg}/>
-            <Card title="Diseño" image={designImg}/>
-            <Card title="Desarrollo Web" image={webImg}/>
-            <Card title="Aplicaciones Móviles" image={mobileImg}/>
-            <Card title="Consultoría" image={consultingImg}/>
-            <Card title="Soporte" image={supportImg}/>
-        </div>
+    <div className="w-3/4 mx-auto mb-24">
+      <div id="services" className="flex justify-center items-center mb-24 text-4xl text-gray-900 font-semibold">
+        Servicios
+      </div>
+      <div className="flex flex-wrap justify-center gap-4"> {/* Cambiado a flex y flex-wrap */}
+        <Card title="Software" image={softwareImg} text="Creamos aplicaciones de escritorio personalizadas que maximizan la eficiencia de tu negocio. Nuestro equipo desarrolla soluciones robustas y seguras." />
+        <Card title="Desarrollo Web" image={webImg} text="Diseñamos páginas web innovadoras y funcionales que destacan tu marca en el entorno digital. Nuestro equipo se especializa en crear sitios web atractivos y optimizados para la experiencia del usuario." />
+        <Card title="Aplicaciones Móviles" image={mobileImg} text="Desarrollamos aplicaciones móviles que conectan y empoderan a los usuarios. Nuestro equipo se dedica a crear soluciones intuitivas y funcionales, adaptadas a las necesidades específicas de tu negocio." />
+      </div>
     </div>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 
-const Card = ({ title, image }) => {
+const Card = ({ title, image, text }) => {
   return (
-    <div className="flex justify-center isolate px-6 pt-70 lg:px-8 border-r-4">
-      <div className="mx-5">
-        <div className="m-2">
-          <div className="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-            <div className="px-6 py-4">
-              <h2 className="text-center text-xl font-bold text-gray-600">{title}</h2>
-            </div>
-            <div className="px-6 py-4">
-              <img className="h-40 w-40 p-4" src={image} alt={title} />
-            </div>
-          </div>
-        </div>
+    <div className="flex flex-col justify-between items-center max-w-sm mb-40 bg-white shadow-lg rounded-lg overflow-hidden h-140"> {/* Altura fija */}
+      <div className="px-6 py-4">
+        <h2 className="text-center text-xl font-bold text-gray-700">{title}</h2>
+      </div>
+      <div className="px-6 mx-3 text-gray-600 tracking-wide">
+        {text}
+      </div>
+      <div className="px-6 py-4 flex justify-center items-center">
+        <img className="h-40 w-40 p-4" src={image} alt={title} />
       </div>
     </div>
   );
