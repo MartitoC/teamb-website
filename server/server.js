13a79de07723fa9e19485cf90a86d46a3e00d930
HectorJGE <hector.gimenez.escurra@gmail.com>
2024-10-14 11:41:56 -0300

    correo

---
+++ server/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config(); 
+const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+
+const app = express();
+app.use(cors({
+  origin: 'http://teamb.com.py:3000'
+}));
+app.use(express.json());
+
+app.post('/api/send-email', async (req, res) => {
+  const { nombre, email, mensaje, telefono } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.zoho.com',
+    port: 587,
+    secure: false, 
+    auth: {
+      user: process.env.ZOHO_EMAIL,
+      pass: process.env.ZOHO_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.ZOHO_EMAIL, 
+    to: process.env.ZOHO_EMAIL, 
+    subject: `Nuevo mensaje de ${nombre}`,
+    text: `Nombre: ${nombre}\nEmail: ${email}\nMensaje: ${mensaje}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).send('Mensaje enviado');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error al enviar el mensaje');
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Servidor en puerto ${PORT}`));
--- website/src/Pages/Contacto.jsx
+++ website/src/Pages/Contacto.jsx
@@ -2,6 +2,33 @@
 import React, { useState } from 'react';
 
 const Contacto = () => {
+  const [formData, setFormData] = useState({
+    nombre: '',
+    email: '', 
+    mensaje: '',
+    telefono: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://teamb.com.py:5000/api/send-email', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      console.log('Mensaje enviado!');
+      setFormData({ nombre: '', email: '', mensaje: '', telefono: ''});
+    } else {
+      alert('Error al enviar el mensaje.');
+    }
+  };
 
   return (
     <div id='contacto'>
@@ -9,16 +36,18 @@ const Contacto = () => {
         <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
           <p className="mx-auto mt-2 max-w-lg text-pretty text-center text-4xl font-medium tracking-tight text-gray-950 sm:text-5xl">Contacto</p>
         </div>
-        <form autoComplete="off">
+        <form onSubmit={handleSubmit} autoComplete="off">
           <div className='p-6 w-full max-w-3xl my-6 mx-auto shadow bg-white'>
             <div className="">
               <div className="col-span-full">
-                <label htmlFor="nombre-completo" className="block text-sm font-medium leading-6 text-gray-900">Nombre Completo *</label>
+                <label htmlFor="nombre" className="block text-sm font-medium leading-6 text-gray-900">Nombre Completo *</label>
                 <div className="mt-2">
                   <input 
                     type="text"
-                    name="nombre-completo" 
-                    id="nombre-completo" 
+                    name="nombre" 
+                    id="nombre" 
+                    value={formData.nombre}
+                    onChange={handleChange}
                     className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-blue-0 sm:text-sm sm:leading-6"
                   />
                 </div>
@@ -27,12 +56,14 @@ const Contacto = () => {
             <div className="grid grid-cols-2 gap-4">
               <div className="mt-2">
                 <div className="col-span-full">
-                  <label htmlFor="correo-electronico" className="block text-sm font-medium leading-6 text-gray-900">Correo electrónico *</label>
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Correo electrónico *</label>
                   <div className="mt-2">
                     <input 
                       type="email" 
-                      name="correo-electronico" 
-                      id="correo-electronico" 
+                      name="email" 
+                      id="email" 
+                      value={formData.email}
+                      onChange={handleChange}
                       className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-blue-0 sm:text-sm sm:leading-6"/>
                   </div>
                 </div>
@@ -41,7 +72,14 @@ const Contacto = () => {
                 <div className="col-span-full">
                   <label htmlFor="telefono" className="block text-sm font-medium leading-6 text-gray-900">Teléfono (Opcional)</label>
                   <div className="mt-2">
-                    <input type="text" name="telefono" id="telefono" className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-blue-0 sm:text-sm sm:leading-6"/>
+                    <input 
+                      type="number" 
+                      name="telefono" 
+                      id="telefono" 
+                      value={formData.telefono}
+                      onChange={handleChange}
+                      className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-blue-0 sm:text-sm sm:leading-6"
+                    />
                   </div>
                 </div>
               </div>
@@ -50,12 +88,20 @@ const Contacto = () => {
               <div className="col-span-full">
                 <label htmlFor="mensaje" className="block text-sm font-medium leading-6 text-gray-900">Mensaje *</label>
                 <div className="mt-2">
-                  <textarea id="mensaje" name="mensaje" rows="3" placeholder="Escribe aquí..." className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-blue-0 sm:text-sm sm:leading-6"></textarea>
+                  <textarea 
+                    id="mensaje" 
+                    name="mensaje" 
+                    rows="4" 
+                    placeholder="Escribe aquí..." 
+                    value={formData.mensaje}
+                    onChange={handleChange}
+                    className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-blue-0 sm:text-sm sm:leading-6">
+                  </textarea>
                 </div>
               </div>
             </div>
             <div className='mt-4 flex justify-center'>
-              <button className="mx-auto bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
+              <button type='submit' className="mx-auto bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-300">
                 Enviar
               </button>
             </div>
